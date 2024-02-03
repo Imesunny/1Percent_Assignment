b2a7ed6df0b8601b7ffc67acd3aa1ef2a9f22dc8
Sunny Anand <rvrahulvermaonly1@gmail.com>
2024-02-03 10:39:04 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const mongoDBConnection = require("./config/db");
 const userRouter = require("./routes/user.routes");
+const TodoModel = require("./models/tasks.model");
+const todoRouter = require("./routes/task.routes");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
+const toggleRouter = require("./routes/toggle.routes");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.json());
 const PORT = parseInt(process.env.PORT);
@@ -10,8 +20,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to the app!" });
 });
 
-
-app.use('/user', userRouter);
+app.use("/user", userRouter);
+app.use("/todo", todoRouter);
+app.use("/toggle", toggleRouter);
 
 app.listen(PORT, async () => {
   try {
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,53 +5,59 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const UserModel = require("../models/user.model");
 
-userRouter.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const user = await UserModel.findOne({ email });
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const is_User = await UserModel.findOne({ email: email });
+
+  if (is_User) {
+    const hashed_password = is_User.password;
+    bcrypt.compare(password, hashed_password, async (err, result) => {
+      if (result) {
+        const token = jwt.sign(
+          {
+            UserID: is_User._id,
+          },
+          "heyDev"
+        );
+
+        return res.json({ message: "Login Success", token: token });
+      } else {
+        return res.json({
+          message: "Invalid crediantials, Try Again!!",
+        });
       }
-  
-      const passwordMatch = await bcrypt.compare(password, user.password);
-  
-      if (!passwordMatch) {
-        return res.status(401).json({ message: 'Invalid password' });
-      }
-  
-      res.status(200).json({ message: 'Login successful' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+    });
+  } else {
+    return res.json({ message: "User not found!, Try Signing in again" });
+  }
+});
+
+userRouter.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const existingUser = await UserModel.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
     }
-  });
-  
-  userRouter.post('/signup', async (req, res) => {
-    const { username, email, password } = req.body;
-  
-    try {
-      const existingUser = await UserModel.findOne({ email });
-  
-      if (existingUser) {
-        return res.status(409).json({ message: 'User already exists' });
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const newUser = new UserModel({
-        username,
-        email,
-        password: hashedPassword,
-      });
-  
-      await newUser.save();
-  
-      res.status(201).json({ message: 'Signup successful' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
-  
-  module.exports=userRouter;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = userRouter;
