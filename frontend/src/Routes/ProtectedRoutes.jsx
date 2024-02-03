820b8130706d331be1fdd6d4cdf1bf32c139e7ba
Sunny Anand <rvrahulvermaonly1@gmail.com>
2024-02-03 10:23:32 +0530

    Done

--- frontend/src/Routes/ProtectedRoutes.jsx
+++ frontend/src/Routes/ProtectedRoutes.jsx
@@ -1,25 +1,23 @@
 import React, { Component, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProtectedRoutes = (props) => {
   const { Component } = props;
   const navigate = useNavigate();
-  const notify = (message) => toast(message);
 
   useEffect(() => {
     let login = localStorage.getItem("token");
-    console.log(login, 'from protected component');
+    console.log(login, "from protected component");
     if (!login) {
-      const is_Alert= "Please Login First"
+      const is_Alert = "Please Login First";
       toast.error(is_Alert, {
-        position: "top-center"
+        position: "top-center",
       });
-      navigate('/login');
+      navigate("/login");
     }
-  },[]);
+  }, []);
 
   return (
     <div>
