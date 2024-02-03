f96e95ee1078e74b8367903ff809878aa5ad20d5
Sunny Anand <rvrahulvermaonly1@gmail.com>
2024-02-03 10:23:06 +0530

    All Done

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -4,44 +4,19 @@ import SignUp from "../Pages/SignUp";
 import Login from "../Pages/Login";
 import HomePage from "../Pages/HomePage";
 import AddUser from "../Components/AddUser";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path ='/addUser' element={<AddUser/>}/>
+
+      <Route path="/home" element={<ProtectedRoutes Component={HomePage} />} />
+
+      <Route path="/addUser" element={<AddUser />} />
     </Routes>
   );
 };
 
 export default AllRoutes;
-
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import SignUp from "../Pages/SignUp";
-// import Login from "../Pages/Login";
-// import HomePage from "../Pages/HomePage";
-// import ProtectedRoute from "./ProtectedRoutes";
-// import AddUser from "../Components/AddUser";
-
-// const AllRoutes = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<SignUp />} />
-//       <Route path="/login" element={<Login />} />
-//       <Route
-//         path="*"
-//         element={
-//           <ProtectedRoute>
-//             <HomePage />
-//           </ProtectedRoute>
-//         }
-//       />
-//       <Route path ='/addUser' element={<AddUser/>}/>
-//     </Routes>
-//   );
-// };
-
-// export default AllRoutes;
--- frontend/src/Routes/ProtectedRoutes.jsx
+++ frontend/src/Routes/ProtectedRoutes.jsx
@@ -1,9 +1,32 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import React, { Component, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const ProtectedRoute = () => {
-  const isAuthenticated = localStorage.getItem('token');
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+const ProtectedRoutes = (props) => {
+  const { Component } = props;
+  const navigate = useNavigate();
+  const notify = (message) => toast(message);
+
+  useEffect(() => {
+    let login = localStorage.getItem("token");
+    console.log(login, 'from protected component');
+    if (!login) {
+      const is_Alert= "Please Login First"
+      toast.error(is_Alert, {
+        position: "top-center"
+      });
+      navigate('/login');
+    }
+  },[]);
+
+  return (
+    <div>
+      {/* abc -for check */}
+      <Component />
+    </div>
+  );
 };
 
-export default ProtectedRoute;
+export default ProtectedRoutes;
